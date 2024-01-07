100feeb54cddf8bb5d44c6008a24a8ad0ad9e37b
Gregor Henrich <gregor.henrich@deutschebahn.com>
2024-01-07 23:19:23 +0100

    feat: implement a basic server without any routes

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
